b7fd0d4ee35a8991a1fdaf954f7b3cb39de93f3f
Kadaparthi Saikiran <99681130+Saikiran-kadaparthi@users.noreply.github.com>
2025-09-19 20:30:16 +0530

    Add files via upload

---
+++ day 34 - C problems from HackerRank/Functions in C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int greatest = a;
+    if(a > b && a > c && a > d)
+    {
+        greatest = a;
+    }
+    else if (b > a && b > c && b > d)
+    {
+        greatest = b;
+    }
+    else if (c > a && c > b && c > d)
+    {
+        greatest = c;
+    }
+    else
+    {
+        greatest = d;
+    }
+    
+    return greatest;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
